6a575c0fa360043824ee48e0dea3f7417117f3c5
Ayush783 <ayushc45xxx@gmail.com>
2021-11-06 18:08:03 +0530

    added two more views

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('v1/', views.welcome),
+    path('', views.welcome),
+    path('register/<str:id>/', views.register),
+    path('user/<str:id>/data/', views.userdata),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
+from django.http.response import JsonResponse
 
 # Create your views here.
 
 def welcome(request):
-    return HttpResponse('Welcome to bahi khata api')
+    return JsonResponse(data={'message':'Welcome to bahi khata api'})
+
+def register(request,id):
+    pass
+
+def userdata(request,id):
+    pass
